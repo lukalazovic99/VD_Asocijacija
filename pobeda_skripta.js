174718ba178a5f6c371912e848037578ae1d6180
lukalazovic99 <luka.lazovic7@gmail.com>
2022-05-31 19:28:38 +0200

    Update pobeda_skripta.js

--- pobeda_skripta.js
+++ pobeda_skripta.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
     let poeni1= localStorage.getItem("poeni1");
     let poeni2= localStorage.getItem("poeni2");
-    $('#zapocni').click(function(){
-        window.location.replace('asocijacije-igra.html');
-    })
+    
+    poeni1=parseInt(poeni1,10);
+    poeni2=parseInt(poeni2,10);
     document.getElementById("poeni11").innerHTML = poeni1;
     document.getElementById("poeni22").innerHTML = poeni2;
+
     if(poeni1>poeni2)document.getElementById("pobednik").innerHTML = 'Igrac 1 Je Pobednik'
     if(poeni1<poeni2)document.getElementById("pobednik").innerHTML = 'Igrac 2 Je Pobednik'
     if(poeni1==poeni2)document.getElementById("pobednik").innerHTML = 'Ishod Je Neresen'
+
+    $('#zapocni').click(function(){
+        window.location.replace('asocijacije-igra.html');
+    })
 });
